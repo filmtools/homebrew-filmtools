06c78016cbc83ad6e382c9cd738981d481cbe72c
Carsten Witt <412560+tomkyle@users.noreply.github.com>
2017-12-04 11:04:26 +0100

    Bump version number

--- curvefit.rb
+++ curvefit.rb
@@ -1,8 +1,8 @@
 class Curvefit < Formula
   desc "Finds X/Y value for a given Y/X by polynomial interpolation."
   homepage "https://github.com/filmspeed/curvefit"
-  url "https://github.com/filmspeed/curvefit/archive/1.0.5.tar.gz"
-  sha256 "cd787d6c94dc982a1a6f8e0923e8aeeff83487e89de62acc21c8a4fc5ae2208f"
+  url "https://github.com/filmspeed/curvefit/archive/1.0.6.tar.gz"
+  sha256 "782c68d8f26a453acd904872f17e35212318de010170265a5852569c98ded1b8"
 
   bottle :unneeded
 
